451809b8399288e9ceddcfcd718cc27510ebdb66
Connell Torek <connellsensational@gmail.com>
2020-01-15 17:01:26 -0500

    Added Limelight to factory

--- src/main/java/frc/robot/Factory.java
+++ src/main/java/frc/robot/Factory.java
@@ -11,8 +11,10 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
+
 import frc.robot.subsystems.DriveSystem;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.LimelightSubsystem;
 
 /**
  * Add your docs here.
@@ -20,6 +22,7 @@ import frc.robot.subsystems.ExampleSubsystem;
 public class Factory {
     private static DriveSystem driveSystem = null;
     private static ExampleSubsystem exampleSS = null;
+    private static LimelightSubsystem limelightSystem = null;
 
 
     public static DriveSystem getDrive(){
@@ -34,5 +37,12 @@ public class Factory {
         if(exampleSS == null)
             exampleSS = new ExampleSubsystem();
         return exampleSS;
+    }
+
+    public static LimelightSubsystem getLimelight() {
+        if(limelightSystem == null) {
+            limelightSystem = new LimelightSubsystem();
+        }
+        return limelightSystem;
     }
 }
