92a25e67790eca8b22b03b32a17088834ad9c562
Jacarmichael712 <mrmafioso712@gmail.com>
2021-03-20 15:03:03 -0400

    Added alternating signums for translations

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -286,12 +286,12 @@ public class RobotContainer {
       List.of(
         // Here is where you add interior waypoints
         // First point in the translation is the vertical position and second is the horizontal position
-        new Translation2d(getNavPointVertical(2.0) + centerRobotToIntakeY(firstPointAngle), getNavPointHorizontal(0.0) + centerRobotToIntakeX(firstPointAngle)),
-        new Translation2d(getNavPointVertical(4.0) + centerRobotToIntakeY(secondPointAngle), getNavPointHorizontal(1.0) + centerRobotToIntakeX(secondPointAngle)),
-        new Translation2d(getNavPointVertical(5.0) + centerRobotToIntakeY(thirdPointAngle), getNavPointHorizontal(-2.0) + centerRobotToIntakeX(thirdPointAngle))
+        new Translation2d(getNavPointVertical(2.0) - centerRobotToIntakeY(firstPointAngle), getNavPointHorizontal(0.0) + centerRobotToIntakeX(firstPointAngle)),
+        new Translation2d(getNavPointVertical(4.0) - centerRobotToIntakeY(secondPointAngle), getNavPointHorizontal(1.0) + centerRobotToIntakeX(secondPointAngle)),
+        new Translation2d(getNavPointVertical(5.0) - centerRobotToIntakeY(thirdPointAngle), getNavPointHorizontal(-2.0) + centerRobotToIntakeX(thirdPointAngle))
       ), 
       // The final end point of the trajectory path
-      new Pose2d(getNavPointVertical(10.0) + Constants.centerRobotToIntakeMeters, getNavPointHorizontal(-2.0), new Rotation2d(0)), 
+      new Pose2d(getNavPointVertical(10.0), getNavPointHorizontal(-2.0), new Rotation2d(0)), 
       config
     ); 
   }
