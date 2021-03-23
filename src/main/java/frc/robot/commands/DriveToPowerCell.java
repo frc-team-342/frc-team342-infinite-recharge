f17c26c24553a5ad339708c13c47113f251aab23
Jacarmichael712 <Mrmafioso712@gmail.com>
2021-03-23 16:25:57 -0400

    waht

--- src/main/java/frc/robot/commands/DriveToPowerCell.java
+++ src/main/java/frc/robot/commands/DriveToPowerCell.java
@@ -12,10 +12,19 @@ import org.photonvision.PhotonTrackedTarget;
 import frc.robot.subsystems.DriveSystem;
 import frc.robot.subsystems.IntakeAndOutake;
 import frc.robot.Factory;
+import frc.robot.RobotContainer;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import edu.wpi.first.wpilibj.Joystick;
 
 public class DriveToPowerCell extends CommandBase {
   DriveSystem drive;
   IntakeAndOutake intake;
+  private Joystick joy;
+
+  private double X;
+  private double Y;
   
   PhotonCamera camera;
   double angle; // degrees from center of robot
@@ -23,12 +32,13 @@ public class DriveToPowerCell extends CommandBase {
 
   /** Creates a new DriveToPowerCell. */
   public DriveToPowerCell() {
-    camera = new PhotonCamera("camera1");
+    camera = new PhotonCamera("HD_USB_Camera");
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    joy = RobotContainer.getJoy();
     drive = Factory.getDrive();
     intake = Factory.getIntakeOutake();
   }
@@ -36,15 +46,30 @@ public class DriveToPowerCell extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    X = joy.getX();
+    Y = joy.getY();
+
+    if (Math.abs(X) < 0.15) {
+      X = 0.0;
+      SmartDashboard.putString("X Deadzone", "X is in deadzone!");
+    } else
+      SmartDashboard.putString("X Deadzone", "X is not in deadzone!");
+
+    if (Math.abs(Y) < 0.15) {
+      Y = 0.0;
+      SmartDashboard.putString("Y Deadzone", "Y is in deadzone!");
+    } else
+      SmartDashboard.putString("Y Deadzone", "Y is not in deadzone!");
+
     PhotonPipelineResult result = camera.getLatestResult();
     if (result.hasTargets()) {
       System.out.println("Has Target");
-      PhotonTrackedTarget target = result.getBestTarget();
+      PhotonTrackedTarget target = result.getTargets().get(0);
       
       // turn towards ball
       angle = target.getYaw();
-      System.out.println("Yaw of Powercell " + angle + "\nRobot Angle " + drive.getGyro() + "\nRobot Angle - Yaw of Powercell " + (drive.getGyro() - angle));
-      //drive.autoRotate(drive.getGyro() - angle); // autorotate takes field-relative degree, not robot
+      System.out.println("Yaw of Powercell " + angle + "\nRobot Angle " + drive.getGyro() + "\nRobot Angle - Yaw of Powercell " + (drive.getGyro() + angle));
+      drive.driveWithTargeting(0, 0, angle); // autorotate takes field-relative degree, not robot
       
       // drive to ball
       //drive.Drive(0.0, 0.0, 0.0);
