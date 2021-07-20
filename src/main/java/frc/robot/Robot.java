7ac86eac8d8455158cd480def2db3a0762b771b7
connellsensational@gmail.com <connelltorek@gmail.com>
2021-07-20 17:24:08 -0400

    Fixed an error were the autonomous did not run

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -109,6 +109,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     //autoDrive.schedule();
+    m_robotContainer.getAutonomousCommand().schedule();
   }
 
   /**
